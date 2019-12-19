8f34d0b2052c966ea21faf72e6f9d95d0fc6b03d
Benjamin Horns <benhorns@gmail.com>
2019-12-19 17:30:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var string = "HELLO"
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string){
+console.log(string.toUpperCase())
+}
+
+
+function whisper(string){
+return string.toLowerCase()
+}
+
+function logWhisper(string){
+console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+}

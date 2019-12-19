662749c85f1b2849dc04bc498d42894414ddc3a4
Benjamin Horns <benhorns@gmail.com>
2019-12-19 17:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,13 @@ console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
+  if (string ==="hello"){
+  console.log("I can't hear you!")
+} else if (string ==="HELLO"){
+  console.log("YES INDEED!")
+} else (string === "I love you, Grandma.") {
+  console.log("I love you, too.")
+}
+  
+   
 }
